afd132da27dc874c11f6232a5dfe046c7febbe78
Lonnie Shankling <lonnie.shankling@gmail.com>
2026-02-20 11:28:32 -0800

    fix: add babel.config.js with module-resolver for path aliases in EAS builds

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+            '~backend': './backend',
+            '~encore': './backend/encore.gen',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
